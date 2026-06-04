0804cc78f6cbb7ca70bf786e1934b0f4ef3b0871
Andrei Popovici <me@andreipopovici.net>
2026-06-04 23:47:49 +0300

    fix: preserve mixer color temperature bounds

--- custom_components/color_temperature_light_mixer/light/color_temperature_mixer.py
+++ custom_components/color_temperature_light_mixer/light/color_temperature_mixer.py
@@ -158,8 +158,8 @@ class ColorTemperatureMixerLight(LightGroup, ColorTemperatureMixerEntity, Restor
         )
 
         brightness_calculator = BrightnessCalculator(
-            self.min_color_temp_kelvin,
-            self.max_color_temp_kelvin,
+            self.__warm_light.color_temp_kelvin,
+            self.__cold_light.color_temp_kelvin,
             target_temp_kelvin,
             target_brightness,
             priority,
@@ -236,6 +236,8 @@ class ColorTemperatureMixerLight(LightGroup, ColorTemperatureMixerEntity, Restor
         # intentionally exposes color temperature.
         self._attr_supported_color_modes = {ColorMode.COLOR_TEMP}
         self._attr_color_mode = ColorMode.COLOR_TEMP
+        self._attr_min_color_temp_kelvin = self.__warm_light.color_temp_kelvin
+        self._attr_max_color_temp_kelvin = self.__cold_light.color_temp_kelvin
 
         states = [state for entity_id in self._entity_ids if (state := self.hass.states.get(entity_id)) is not None]
         on_states = [state for state in states if state.state == STATE_ON]
